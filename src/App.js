d1c6ea97c99e4e073c952629591502705d318825
Reza Merzaie <ghrezam97@gmail.com>
2023-12-01 23:05:31 +0430

    Add the VehicleDetails component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import VehicleDetails from './pages/VehicelDetails';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    // <h1>Hello</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/vehicles/:id" element={<VehicleDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/VehicelDetails.jsx
@@ -0,0 +1,8 @@
+const VehicleDetails = () => {
+  const state = 'Details';
+  return (
+    <h1>{state}</h1>
+  );
+};
+
+export default VehicleDetails;
