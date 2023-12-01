72b70bf44a8b7e1681adce140ab2d683572351e1
Reza Merzaie <ghrezam97@gmail.com>
2023-12-01 23:22:33 +0430

    Create the vehicle details slice

---
+++ src/redux/vehicle_details/vehicleDetailsSlice.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchVehicleDetails = createAsyncThunk(
+  'vehicleDetails/getVehicleDetails',
+  async (vehicleId) => {
+    const response = await fetch(`http://localhost:3000/api/v1/vehicles/${vehicleId}`);
+    const data = await response.json();
+    return data;
+  },
+);
+
+const vehicleDetailsSlice = createSlice({
+  name: 'vehicleDetails',
+  initialState: {
+    details: {},
+    status: 'loading',
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchVehicleDetails.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchVehicleDetails.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.details = action.payload;
+      })
+      .addCase(fetchVehicleDetails.rejected, (state) => {
+        state.status = 'failed';
+      });
+  },
+});
+
+export default vehicleDetailsSlice.reducer;
