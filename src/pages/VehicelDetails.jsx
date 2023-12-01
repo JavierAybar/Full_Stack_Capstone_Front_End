be82a5275ecefef65bb1e80f95fcb1a4c5b6e79e
Reza Merzaie <ghrezam97@gmail.com>
2023-12-02 00:58:56 +0430

    Add reserve btn

--- src/pages/VehicelDetails.jsx
+++ src/pages/VehicelDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { fetchVehicleDetails } from '../redux/vehicle_details/vehicleDetailsSlice';
 
 const VehicleDetails = () => {
@@ -19,12 +19,15 @@ const VehicleDetails = () => {
           <img src={vehicleDetails.image} alt="vehicle" className="img-fluid" />
         </div>
         <div className="vehicle-details-container col pt-5 mt-5">
-          <h2 className="h1 fw-bold">{vehicleDetails.name}</h2>
-          <strong className="w-100 border-bottom d-block">
-            Price:
-            {vehicleDetails.price}
-          </strong>
-          <p>{vehicleDetails.description}</p>
+          <div>
+            <h2 className="h1 fw-bold">{vehicleDetails.name}</h2>
+            <strong className="w-100 border-bottom d-block">
+              Price:
+              {vehicleDetails.price}
+            </strong>
+            <p>{vehicleDetails.description}</p>
+          </div>
+          <Link to="reserve" className="btn btn-success ps-4 pe-4">Reserve</Link>
         </div>
       </div>
     </div>
