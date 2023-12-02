8eaa8484248070be12b2062d4bfd4919ba03cad1
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-12-02 18:48:10 -0300

    Add route for delete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Provider } from 'react-redux';
 import VehicleDetails from './pages/VehicelDetails';
 import VehicleList from './pages/VehicleList';
+import DeleteVehicle from './pages/DeleteVehicle';
 
 import store from './redux/store';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/vehicles" element={<VehicleList />} />
           <Route path="/vehicles/:id" element={<VehicleDetails />} />
+          <Route path="/delete" element={<DeleteVehicle />} />
         </Routes>
       </BrowserRouter>
     </Provider>
