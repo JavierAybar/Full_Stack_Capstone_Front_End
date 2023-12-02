3bb1d2080bc39ca25b57c6948c2bf199c7ca728a
Marc raphael <marcraphael979@gmail.com>
2023-12-02 21:19:24 +0100

    Add reserve reducer to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import vehicleDetailsReducer from './vehicle_details/vehicleDetailsSlice';
+import vehicleDetailsReducer from './reservation/reservSlice';
 
 const store = configureStore({
   reducer: {
     vehicleDetails: vehicleDetailsReducer,
+    reservation: reservSliceReducer
   },
 });
 
