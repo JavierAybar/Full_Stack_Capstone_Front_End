d2c46e01ef064154b3811f36976c75064c5cfb4c
Nurbol <nurbol.sultanov@gmail.com>
2023-12-02 02:09:32 -0800

    Add routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
 import VehicleDetails from './pages/VehicelDetails';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import Logout from './pages/Logout';
 import store from './redux/store';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/vehicles/:id" element={<VehicleDetails />} />
+          <Route path="/login" element={<Login isAuthenticated />} />
+          <Route path="/register" element={<Registration isAuthenticated />} />
+          <Route path="/logout" element={<Logout isAuthenticated />} />
         </Routes>
       </BrowserRouter>
     </Provider>
