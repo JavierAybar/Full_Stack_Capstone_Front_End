7931e8a6d0f93ff441a93f53534e0a148e1ca1fb
Marc raphael <marcraphael979@gmail.com>
2023-12-02 21:20:08 +0100

    Add routes for reverse pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
-import VehicleDetails from './pages/VehicelDetails';
 import store from './redux/store';
+import VehicleDetails from './pages/VehicelDetails';
+import NewReservation from './pages/reservation/NewReservation';
+import NewCarReservation from './pages/reservation/NewCarReservation';
+import MyReservations from './pages/reservation/MyReservations';
 
 function App() {
   return (
@@ -11,6 +16,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/vehicles/:id" element={<VehicleDetails />} />
+          <Route path="/new-reservation" element={<NewReservation />} />
+          <Route path="/car-reservation/:vehicle_id" element={<NewCarReservation />} />
+          <Route path="/my-reservations" element={<MyReservations />} />
         </Routes>
       </BrowserRouter>
     </Provider>
