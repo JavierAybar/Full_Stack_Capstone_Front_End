d85556d3f103a1907586d68b1e0e2d9e3a60fa23
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-12-02 09:53:28 -0300

    Add route for main page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
 import VehicleDetails from './pages/VehicelDetails';
+import VehicleList from './pages/VehicleList';
+
 import store from './redux/store';
 
 function App() {
   return (
-    // <h1>Hello</h1>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
+          <Route path="/vehicles" element={<VehicleList />} />
           <Route path="/vehicles/:id" element={<VehicleDetails />} />
         </Routes>
       </BrowserRouter>
