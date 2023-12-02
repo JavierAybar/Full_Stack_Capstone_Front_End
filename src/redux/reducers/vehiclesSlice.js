3e648eb687a806d82cbd1d03b18a3a8d1154668e
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-12-02 18:49:06 -0300

    Add thunk  and reducer to delete a item

--- src/redux/reducers/vehiclesSlice.js
+++ src/redux/reducers/vehiclesSlice.js
@@ -1,11 +1,16 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'http://localhost:3000/api/v1/vehicles';
+const url = 'http://localhost:3000/api/v1';
 
 export const fetchVehicles = createAsyncThunk('vehicles/fetchVehicles', async () => {
-  const response = await axios.get(url);
+  const response = await axios.get(`${url}/vehicles`);
   return response.data;
+});
+
+export const deleteVehicle = createAsyncThunk('vehicles/deleteVehicle', async (id) => {
+  await axios.delete(`${url}/vehicles/${id}`);
+  return id;
 });
 
 const initialState = {
@@ -18,11 +23,14 @@ const vehiclesSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchVehicles.fulfilled, (state, action) => ({
-      ...state,
-      vehicle: action.payload,
-    }));
+    builder
+      .addCase(fetchVehicles.fulfilled, (state, action) => ({
+        ...state,
+        vehicle: action.payload,
+      }))
+      .addCase(deleteVehicle.fulfilled, (state, action) => {
+        state.vehicle = state.vehicle.filter((vehicle) => vehicle.id !== action.payload);
+      });
   },
 });
-
 export default vehiclesSlice.reducer;
