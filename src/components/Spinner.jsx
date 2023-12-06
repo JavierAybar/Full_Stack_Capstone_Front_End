87edc5e642d46f57b61e76a9ad6f128147292d25
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-12-06 21:38:22 +0200

    Fix linters

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Spinner = () => (
   <div className="flex justify-center items-center">
     <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900" />
