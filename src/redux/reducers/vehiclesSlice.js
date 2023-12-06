1376d05f552a08569aea187d823bd684c8d7459e
Javier Aybar <javi_aybar@hotmail.com>
2023-12-06 13:31:15 -0300

    Revert "Feature/delete item"

--- src/redux/reducers/vehiclesSlice.js
+++ src/redux/reducers/vehiclesSlice.js
@@ -1,21 +1,16 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const url = 'http://localhost:3000/api/v1';
+const url = 'http://localhost:3000/api/v1/vehicles';
 
 export const fetchVehicles = createAsyncThunk('vehicles/fetchVehicles', async () => {
-  const response = await axios.get(`${url}/vehicles`);
+  const response = await axios.get(url);
   return response.data;
-});
-
-export const deleteVehicle = createAsyncThunk('vehicles/deleteVehicle', async (id) => {
-  await axios.delete(`${url}/vehicles/${id}`);
 });
 
 const initialState = {
   vehicle: [],
-  isLoading: false,
-  error: null,
+  isLoading: true,
 };
 
 const vehiclesSlice = createSlice({
@@ -23,22 +18,11 @@ const vehiclesSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(fetchVehicles.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(fetchVehicles.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.vehicle = action.payload;
-      })
-      .addCase(fetchVehicles.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
-      .addCase(deleteVehicle.fulfilled, (state, action) => {
-        state.vehicle = state.vehicle.filter((vehicle) => vehicle.id !== action.meta.arg);
-      });
+    builder.addCase(fetchVehicles.fulfilled, (state, action) => ({
+      ...state,
+      vehicle: action.payload,
+    }));
   },
 });
+
 export default vehiclesSlice.reducer;
