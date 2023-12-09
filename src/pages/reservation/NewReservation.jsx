c9e8756634089899a32f6058d468cb4f62a2170a
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-12-09 15:52:03 -0300

    add styles form new reservation

--- src/pages/reservation/NewReservation.jsx
+++ src/pages/reservation/NewReservation.jsx
@@ -1,7 +1,7 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/jsx-props-no-spreading */
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BsCalendar2Week } from 'react-icons/bs';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 // import { useNavigate } from 'react-router-dom';
@@ -51,68 +51,75 @@ function AddReservationPage() {
 
   // JSX for the component
   return (
-    <div className="formCont reservationCont">
-      {/* Styles */}
-      <style>
-        {`
-          // ... (styles omitted for brevity)
-        `}
-      </style>
+    <div>
+      <h2 className="formTitle">Test Drive</h2>
+      <p className="formDesc">
+        Schedule your test drive today to embark on an exhilarating experience
+        with our high-performance vehicles! We will promptly confirm your reservation
+        and make your supervehicle dream a reality.
+      </p>
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit(onSubmit)}>
+        {/* City Input */}
+        <label htmlFor="city" className="block mb-1 text-sm font-medium text-gray-900 dark:text-back">
+          Select your city
+        </label>
+        <input
+          required
+          id="city"
+          type="text"
+          name="city"
+          placeholder="City"
+          {...register('city', { required: true })}
+          className="pl-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
 
-      {/* Reservation Form */}
-      <form className="reservationForm" onSubmit={handleSubmit(onSubmit)}>
-        <h2 className="formTitle">Test Drive</h2>
-        <p className="formDesc">
-          Schedule your test drive today to embark on an exhilarating experience
-          with our high-performance vehicles! We will promptly confirm your reservation
-          and make your supervehicle dream a reality.
-        </p>
-        <div className="inputCont">
-          {/* City Input */}
+        {/* Date Input */}
+        <label htmlFor="date" className="block mb-1 text-sm font-medium text-gray-900 dark:text-back">
+          Select your date
+        </label>
+        <div className="relative max-w-sm">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+            </svg>
+          </div>
           <input
             required
-            type="text"
-            name="city"
-            placeholder="City"
-            {...register('city', { required: true })}
-            className="formInput"
+            id="date"
+            type="date"
+            name="date"
+            placeholder="Select date"
+            {...register('date', { required: true })}
+            min={new Date().toISOString().split('T')[0]}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
-
-          {/* Date Input */}
-          <div className="inputDate">
-            <input
-              required
-              type="date"
-              name="date"
-              placeholder="Date"
-              {...register('date', { required: true })}
-              min={new Date().toISOString().split('T')[0]}
-              className="formInput dateInput"
-            />
-            <BsCalendar2Week className="calendarIcon" />
-          </div>
-
-          {/* Vehicle Select */}
-          <select
-            required
-            name="vehicle_id"
-            {...register('vehicle_id', { required: true })}
-            className="formInput formSelect"
-          >
-            <option className="vehicleselector" value="">
-              Select a vehicle
-            </option>
-            {vehicles.map((vehicle) => (
-              <option key={vehicle.id} value={vehicle.id} className="vehicleselector">
-                {vehicle.name}
-              </option>
-            ))}
-          </select>
-          {errors.vehicle_id && <span>Select Vehicle Model</span>}
         </div>
 
+        {/* Vehicle Select */}
+
+        <label htmlFor="vehicles" className="block mt-1 mb-1 text-sm font-medium text-gray-900 dark:text-back">
+          Select your vehicle
+        </label>
+        <select
+          id="vehicles"
+          required
+          name="vehicle_id"
+          {...register('vehicle_id', { required: true })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value="">
+            Select a vehicle
+          </option>
+          {vehicles.map((vehicle) => (
+            <option key={vehicle.id} value={vehicle.id} className="vehicleselector">
+              {vehicle.name}
+            </option>
+          ))}
+        </select>
+        {errors.vehicle_id && <span>Select Vehicle Model</span>}
+
         {/* Submit Button */}
-        <button type="submit" className="reservationSubmit">
+        <button type="submit" className="text-white bg-gradient-to-r from-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
           Book Reservation
         </button>
       </form>
