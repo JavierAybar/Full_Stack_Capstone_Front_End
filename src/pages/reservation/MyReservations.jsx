e3f79fcb4c9bbd7bf66baddc4f708c07e91319f2
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-12-10 16:33:09 -0300

    Add styles for carousel

--- src/pages/reservation/MyReservations.jsx
+++ src/pages/reservation/MyReservations.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable no-nested-ternary */
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toast } from 'react-hot-toast';
-import { SwishSpinner } from 'react-spinners-kit';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import 'swiper/css';
+import { EffectCoverflow, Navigation } from 'swiper/modules';
 
 import { fetchReservations, deleteReservation } from '../../redux/reservation/reservSlice';
 import { fetchVehicles } from '../../redux/reducers/vehiclesSlice';
@@ -18,18 +22,15 @@ function UserReservation() {
   const vehicles = useSelector((state) => state.vehicle.vehicle);
   const authUser = useSelector((state) => state.auth.user.data);
   console.log(authUser.id);
-  const [loading, setLoading] = useState(true);
 
   /**
    * useEffect Hook
    * Fetches reservations and vehicles when the component mounts.
    */
   useEffect(() => {
-    setLoading(true);
     // Fetch reservations and vehicles and set loading to false when done
     dispatch(fetchReservations())
-      .then(() => dispatch(fetchVehicles()))
-      .finally(() => setLoading(false));
+      .then(() => dispatch(fetchVehicles()));
   }, [dispatch]);
 
   /**
@@ -67,63 +68,94 @@ function UserReservation() {
     return reservation.user_id === 2 && vehicleExists;
   });
 
+  const predefinedColors = ['bg-red-500', 'bg-gray-300', 'bg-orange-400', 'bg-cyan-600'];
+
+  // Índice para rastrear el color actual
+  let colorIndex = 0;
+
+  // Función para obtener el próximo color y actualizar el índice
+  const getNextColor = () => {
+    const color = predefinedColors[colorIndex];
+    colorIndex = (colorIndex + 1) % predefinedColors.length;
+    return color;
+  };
+
   // Render the component.
   return (
-    <div className="myReservationsCont">
-      {loading ? (
-        // Display loading spinner while data is being fetched
-        <SwishSpinner size={50} frontColor="#98be18" loading />
-      ) : (
-        // Display user's Test Drive reservations
-        <div className="myReservations">
-          <div className="header">
-            <h1 className="title">Tests Drive</h1>
-            <p className="desc">Here you can see all your Test Drive reservations.</p>
-            <div className="divider" />
-          </div>
+    <div className="container p-[0px]">
+      <div className="flex flex-col items-center pb-[10px] pt-[70px]">
+        <h1 className="text-4xl font-extrabold LINE">Test Drives</h1>
+        <p className="font-semibold text-stone-400">Here you can see all your Test Drive reservations.</p>
+      </div>
 
-          {/* Swiper component to display user's Test Drive reservations */}
-          <div direction="vertical" className="reservationSwiper">
-            {userReservations.map((reservation) => (
-              <div className="reservationSlide" key={reservation.id}>
-                <div style={{
-                  border: '1px solid red', textAlign: 'center', borderRadius: '10px',
-                }}
-                >
-                  {/* Display vehicle information for each reservation */}
-                  {['Name', 'Price', 'Description'].map((attr) => (
+      {/* Swiper component to display user's Test Drive reservations */}
+      <Swiper
+        effect="coverflow"
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Navigation]}
+        className="swiper_container"
+      >
+        {userReservations.map((reservation) => (
+          <SwiperSlide key={reservation.id} style={{ width: '51%', display: 'flex' }}>
+            <div className="shadow-xl card xl:w-[32rem] w-[40rem] bg-base-100  text-center mt-auto" key={reservation.id}>
+              <div className="flex flex-col items-center">
+                <figure className={`rounded-full w-[300px] h-[300px] flex items-center ${getNextColor()}`}><img src={getVehicleProperty(reservation.vehicle_id, 'image')} alt="car" className="xl:h-[200px] h-[250px]" /></figure>
+                <div className="card-body">
+                  {['Name', 'Price'].map((attr) => (
                     <div className="reservationAttr" key={attr}>
-                      <p className="attrName">
+                      <h2 className="mb-0 text-2xl font-bold">
                         {attr}
                         :&nbsp;
                         {getVehicleProperty(reservation.vehicle_id, attr.toLowerCase())}
-                      </p>
+                      </h2>
                     </div>
                   ))}
-                  <p>
+                  <p className="mb-0 text-lg">
                     Date:&nbsp;
                     {reservation.date}
                   </p>
-                  <p>
+                  <p className="mb-0 text-lg ">
                     City:&nbsp;
                     {reservation.city}
                   </p>
-                  <img src={getVehicleProperty(reservation.vehicle_id, 'image')} alt="car" className="xl:h-[200px] h-[250px]" />
-                  <button
-                    type="button"
-                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                    onClick={() => confirmDelete(reservation.id, getVehicleProperty(reservation.vehicle_id, 'name'))}
-                  >
-                    Cancel Reservation
-                  </button>
                 </div>
+                <button
+                  type="button"
+                  className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  onClick={() => confirmDelete(reservation.id, getVehicleProperty(reservation.vehicle_id, 'name'))}
+                >
+                  Cancel Reservation
+                </button>
               </div>
-            ))}
+            </div>
+          </SwiperSlide>
+        ))}
+        <div className="slider-controler">
+          <div className="xl:w-[70px] rounded-full swiper-button-prev slider-arrow bg-lime-400 ">
+            <svg className="w-[10px] h-[10px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 16">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7.293 1.707 1.707 7.293a1 1 0 0 0 0 1.414l5.586 5.586A1 1 0 0 0 9 13.586V2.414a1 1 0 0 0-1.707-.707Z" />
+            </svg>
+          </div>
+          <div className=" xl:w-[70px] rounded-full swiper-button-next slider-arrow bg-lime-400 ">
+            <svg className="w-[10px] h-[10px] text-gray-800 dark:text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 16">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m2.707 14.293 5.586-5.586a1 1 0 0 0 0-1.414L2.707 1.707A1 1 0 0 0 1 2.414v11.172a1 1 0 0 0 1.707.707Z" />
+            </svg>
           </div>
         </div>
-      )}
+      </Swiper>
     </div>
   );
 }
-
 export default UserReservation;
--- src/pages/reservation/NewReservation.jsx
+++ src/pages/reservation/NewReservation.jsx
@@ -7,6 +7,7 @@ import { toast } from 'react-hot-toast';
 // import { useNavigate } from 'react-router-dom';
 import { fetchVehicles } from '../../redux/reducers/vehiclesSlice';
 import { addReservation } from '../../redux/reservation/reservSlice';
+import image from '../../assets/mercedes-reservation.png';
 
 function AddReservationPage() {
   // Hooks and state
@@ -48,81 +49,88 @@ function AddReservationPage() {
       // navigate('/my-reservations');
     });
   };
-
+  // , backgroundSize: 'cover', backgroundPosition: 'center'
   // JSX for the component
   return (
-    <div>
-      <h2 className="formTitle">Test Drive</h2>
-      <p className="formDesc">
-        Schedule your test drive today to embark on an exhilarating experience
-        with our high-performance vehicles! We will promptly confirm your reservation
-        and make your supervehicle dream a reality.
-      </p>
-      <form className="max-w-sm mx-auto" onSubmit={handleSubmit(onSubmit)}>
-        {/* City Input */}
-        <label htmlFor="city" className="block mb-1 text-sm font-medium text-gray-900 dark:text-back">
-          Select your city
-        </label>
-        <input
-          required
-          id="city"
-          type="text"
-          name="city"
-          placeholder="City"
-          {...register('city', { required: true })}
-          className="pl-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
+    <div className="w-full bg-center bg-cover " style={{ backgroundImage: `url(${image})` }}>
+      <div className="flex flex-col items-center 2xl:pt-[80px] lg:pt-[160px] pt-[130px] w-full h-screen bg-opacity-80 bg-lime-500 backdrop-opacity-60">
+        <h2 className="mb-3 text-lg font-extrabold tracking-widest text-white sm:text-4xl">BOOK A VEHICLE TEST-RIDE</h2>
+        <hr className="w-[60%] text-white border-t-4 mb-3" />
+        <p className="sm:w-[45%] w-[90%] mb-10 text-sm font-semibold text-white sm:text-lg">
+          There are 34 different versions of the Vespa. Today five series are
+          in production: the classic manual transmission PX and the modern CVT transmission S,
+          LX, GT and GTS. We have showrooms all over the globe which some include test-riding
+          facilities. If you wish to find out if a test-ride is available in your area, please use
+          the selector below.
+        </p>
+        <div className="mx-auto ">
+          <form className="p-6 bg-white rounded shadow-lg sm:w-[430px] w-[340px]" onSubmit={handleSubmit(onSubmit)}>
+            {/* City Input */}
+            <label htmlFor="city" className="block mt-2 mb-2 text-sm font-medium text-gray-800 dark:text-back">
+              Select your city
+            </label>
+            <input
+              required
+              id="city"
+              type="text"
+              name="city"
+              placeholder="City"
+              {...register('city', { required: true })}
+              className="w-full p-2 mb-2 text-gray-900 border rounded focus:outline-none focus:shadow-outline dark:text-gray-500 "
+            />
 
-        {/* Date Input */}
-        <label htmlFor="date" className="block mb-1 text-sm font-medium text-gray-900 dark:text-back">
-          Select your date
-        </label>
-        <div className="relative max-w-sm">
-          <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-            </svg>
-          </div>
-          <input
-            required
-            id="date"
-            type="date"
-            name="date"
-            placeholder="Select date"
-            {...register('date', { required: true })}
-            min={new Date().toISOString().split('T')[0]}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
+            {/* Date Input */}
+            <label htmlFor="date" className="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-back">
+              Select a date
+            </label>
+            <div className="relative ">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                </svg>
+              </div>
+              <input
+                required
+                id="date"
+                type="date"
+                name="date"
+                placeholder="Select date"
+                {...register('date', { required: true })}
+                min={new Date().toISOString().split('T')[0]}
+                className="border rounded w-full text-gray-900 text-sm  ps-10 p-2.5  dark:placeholder-gray-400 dark:text-gray-500 focus:outline-none focus:shadow-outline"
+              />
+            </div>
+
+            {/* Vehicle Select */}
+
+            <label htmlFor="vehicles" className="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-back">
+              Select a vehicle
+            </label>
+            <select
+              id="vehicles"
+              required
+              name="vehicle_id"
+              {...register('vehicle_id', { required: true })}
+              className="border rounded text-gray-900 text-sm  block w-full p-2.5  dark:placeholder-gray-400 dark:text-gray-500 "
+            >
+              <option value="">
+                Select a vehicle
+              </option>
+              {vehicles.map((vehicle) => (
+                <option key={vehicle.id} value={vehicle.id} className="vehicleselector">
+                  {vehicle.name}
+                </option>
+              ))}
+            </select>
+            {errors.vehicle_id && <span>Select Vehicle Model</span>}
+
+            {/* Submit Button */}
+            <button type="submit" className=" w-full mt-4 text-white bg-gradient-to-r from-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+              Book Reservation
+            </button>
+          </form>
         </div>
-
-        {/* Vehicle Select */}
-
-        <label htmlFor="vehicles" className="block mt-1 mb-1 text-sm font-medium text-gray-900 dark:text-back">
-          Select your vehicle
-        </label>
-        <select
-          id="vehicles"
-          required
-          name="vehicle_id"
-          {...register('vehicle_id', { required: true })}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        >
-          <option value="">
-            Select a vehicle
-          </option>
-          {vehicles.map((vehicle) => (
-            <option key={vehicle.id} value={vehicle.id} className="vehicleselector">
-              {vehicle.name}
-            </option>
-          ))}
-        </select>
-        {errors.vehicle_id && <span>Select Vehicle Model</span>}
-
-        {/* Submit Button */}
-        <button type="submit" className="text-white bg-gradient-to-r from-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-          Book Reservation
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
