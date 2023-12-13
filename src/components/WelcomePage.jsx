442e0d430065701bc0a3eee2c87f66a4ea9482c1
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-12-13 14:24:03 -0300

    Add styles for welcome page

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,14 +1,20 @@
+import image from '../assets/mercedes-home.png';
 import { Link } from 'react-router-dom';
 
 const WelcomePage = () => {
   return (
-    <div className="container mx-auto text-center mt-20 pt-20">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Our Vehicle Reservation System</h1>
-      <p className="mb-8">Join us and explore a wide range of vehicles</p>
-      <Link to="/login" className="btn bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2">Login</Link>
-      <Link to="/register" className="btn bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2">Register</Link>
+    <div className="flex items-center justify-center w-full h-screen bg-center bg-no-repeat bg-contain bg-amber-500" style={{ backgroundImage: `url(${image})` }}>
+      <div className="container xl:mt-[-40rem]  2xl:mt-[-30rem] pt-20 mx-auto text-center xl:w-[500px] sm:w-[350px] w-[300px]">
+        <h1 className="mb-4 text-6xl font-extrabold text-lime-400">Welcome to Our Vehicle Reservation System</h1>
+        <p className="mb-8 text-xl font-bold text-white rounded-full bg-amber-400 sm:text-2xl">Join us and explore a wide range of vehicles</p>
+        <Link to="/login" className="min-w-[100px] px-4 py-2 m-2 font-semibold text-white rounded-full bg-lime-500 btn hover:bg-green-700">Login</Link>
+        <Link to="/register" className="min-w-[100px] px-4 py-2 m-2 font-semibold text-white rounded-full bg-lime-500 btn hover:bg-green-700">Register</Link>
+      </div>
     </div>
+
   );
 };
 
+// style={{ backgroundImage: `url(${image})` }}
+// flex flex-col items-center 2xl:pt-[80px] lg:pt-[160px] pt-[130px]
 export default WelcomePage;
