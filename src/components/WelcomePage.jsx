5986506ea448fde7bd650b95b5fb78777eac7adf
Marc raphael <marcraphael979@gmail.com>
2023-12-14 13:22:17 +0100

    Add a blured background in home page

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 const WelcomePage = () => {
   return (
     <div className="flex items-center justify-center w-full h-screen bg-center bg-no-repeat bg-contain bg-amber-500" style={{ backgroundImage: `url(${image})` }}>
-      <div className="container xl:mt-[-40rem]  2xl:mt-[-30rem] pt-20 mx-auto text-center xl:w-[500px] sm:w-[350px] w-[300px]">
-        <h1 className="mb-4 text-6xl font-extrabold text-lime-400">Welcome to Our Vehicle Reservation System</h1>
-        <p className="mb-8 text-xl font-bold text-white rounded-full bg-amber-400 sm:text-2xl">Join us and explore a wide range of vehicles</p>
+      <div className='blured'>
+        <div className="container xl:mt-[-40rem]  2xl:mt-[-30rem] pt-20 mx-auto text-center xl:w-[500px] sm:w-[350px] w-[300px]">
+        <h1 className="mb-4 text-6xl font-extrabold">Welcome to Our Vehicle Reservation System</h1>
+        <p className="mb-8 text-xl font-bold text-white rounded-full sm:text-2xl">Join us and explore a wide range of vehicles</p>
         <Link to="/login" className="min-w-[100px] px-4 py-2 m-2 font-semibold text-white rounded-full bg-lime-500 btn hover:bg-green-700">Login</Link>
         <Link to="/register" className="min-w-[100px] px-4 py-2 m-2 font-semibold text-white rounded-full bg-lime-500 btn hover:bg-green-700">Register</Link>
+        </div>
       </div>
+
     </div>
 
   );
