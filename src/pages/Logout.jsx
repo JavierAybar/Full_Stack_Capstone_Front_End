b1ed568ff33fe8ed965e14e3212328da0e6cf97f
Nurbol <nurbol.sultanov@gmail.com>
2023-12-13 17:47:33 -0800

    Logout redirects to home page

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,17 +1,26 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { logoutUser } from '../redux/slices/authSlice';
 
 const Logout = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    dispatch(logoutUser());
-  }, [dispatch]);
+    dispatch(logoutUser())
+      .then(() => {
+        // Redirect to the Welcome page after successful logout
+        navigate('/');
+      })
+      .catch((error) => {
+        console.error('Logout failed:', error);
+      });
+  }, [dispatch, navigate]);
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <p className="p-4 text-lg font-semibold bg-white rounded shadow">Logged out</p>
+      <p className="p-4 text-lg font-semibold bg-white rounded shadow">Logging out...</p>
     </div>
   );
 };
