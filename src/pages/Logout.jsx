df09ab3624590b59fba1ac34334d97691ce49e9c
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2025-06-12 00:49:20 -0300

    import react

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { logoutUser } from '../redux/slices/authSlice';
