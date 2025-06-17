947d7d5c79f14fb3ca05c1912e3d7e75419b683b
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2025-06-16 23:08:51 -0300

    Add custom breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
       'lg': '1024px',  
       'xl': '1440px',  
       '2xl': '1800px', 
+      'lg-tall': { 'raw': '(min-width: 1024px) and (min-height: 1000px)' },
     }
   },
   plugins: [],
