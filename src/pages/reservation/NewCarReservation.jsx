8a8479529c45b7e5b094ab11d0a69267b3597990
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2025-08-13 16:24:09 -0300

    Final style adjustments to ensure consistency across the entire site

--- src/pages/reservation/NewCarReservation.jsx
+++ src/pages/reservation/NewCarReservation.jsx
@@ -8,7 +8,7 @@ import { useForm } from 'react-hook-form';
 import { fetchVehicles } from '../../redux/reducers/vehiclesSlice';
 import { addReservation } from '../../redux/reservation/reservSlice';
 
-import image from '../../assets/cars_parking.webp';
+import image from '../../assets/car-8647797_1280.webp';
 
 const AddReservationPage = () => {
   const { id } = useParams();
@@ -40,20 +40,20 @@ const AddReservationPage = () => {
   };
 
   return (
-    <div className="w-full bg-center bg-cover " style={{ backgroundImage: `url(${image})` }}>
-      <div className="flex flex-col items-center 2xl:pt-[80px] lg:pt-[160px] pt-[130px] w-full h-screen bg-opacity-80 bg-lime-500 backdrop-opacity-60">
-        <h2 className="mb-3 text-lg font-extrabold tracking-widest text-white sm:text-4xl">BOOK A VEHICLE TEST-RIDE</h2>
-        <hr className="w-[60%] text-white border-t-4 mb-3" />
-        <p className="sm:w-[45%] w-[90%] mb-10 text-sm font-semibold text-white sm:text-lg">
-          There are 34 different versions of the Vespa. Today five series are
-          in production: the classic manual transmission PX and the modern CVT transmission S,
-          LX, GT and GTS. We have showrooms all over the globe which some include test-riding
-          facilities. If you wish to find out if a test-ride is available in your area, please use
-          the selector below.
-        </p>
-        <div className="login-form-div">
-          <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="city" className="block mt-2 mb-2 text-sm font-medium text-gray-800 dark:text-back">
+   <div className="flex flex-col items-center w-full h-screen bg-cover" style={{ backgroundImage: `url(${image})` }}>
+         <div className=" reservation flex flex-col items-center  lg:pt-[120px] pt-[80px] md:pt-[150px] w-full h-screen bg-black/30 backdrop-blur-sm">
+           <h2 className="mb-6 text-4xl font-extrabold tracking-widest text-center text-white xl:text-5xl">BOOK A VEHICLE TEST-RIDE</h2>
+           <hr className="w-[60%] text-white border-t-4 mb-6" />
+           <p className="sm:w-[45%] w-[90%] mb-10 lg:text-[1.7rem] font-thin text-white sm:text-2xl text-center pb-16">
+             There are 34 different versions of the Vespa. Today five series are
+             in production: the classic manual transmission PX and the modern CVT transmission S,
+             LX, GT and GTS. We have showrooms all over the globe which some include test-riding
+             facilities. If you wish to find out if a test-ride is available in your area, please use
+             the selector below.
+           </p>
+           <div className="w-full max-w-2xl p-8 mx-auto shadow-2xl bg-black/1 backdrop-blur-md rounded-xl">
+           <form className="w-full login-form" onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="city" className="block mb-2 text-2xl font-semibold text-white xl:text-3xl">
               Select your city
             </label>
             <input
@@ -63,15 +63,15 @@ const AddReservationPage = () => {
               name="city"
               placeholder="City"
               {...register('city', { required: true })}
-              className="w-full p-2 mb-2 text-gray-900 border rounded focus:outline-none focus:shadow-outline dark:text-gray-500 "
+              className="w-full p-4 mb-4 text-lg text-gray-900 border border-gray-300 rounded-lg xl:text-2xl focus:outline-none focus:ring-4 focus:ring-red-400"
             />
 
-            <label htmlFor="date" className="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-back">
+            <label htmlFor="date" className="block mb-2 text-2xl font-semibold text-white xl:text-3xl">
               Select a date
             </label>
-            <div className="relative ">
+             <div className="relative mb-4">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <svg className="w-6 h-6 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                 </svg>
               </div>
@@ -83,11 +83,14 @@ const AddReservationPage = () => {
                 placeholder="Select date"
                 {...register('date', { required: true })}
                 min={new Date().toISOString().split('T')[0]}
-                className="border rounded w-full text-gray-900 text-sm  ps-10 p-2.5  dark:placeholder-gray-400 dark:text-gray-500 focus:outline-none focus:shadow-outline"
+                className="w-full p-4 text-lg text-gray-900 border rounded-lg xl:text-2xl ps-12 focus:outline-none focus:ring-4 focus:ring-red-400"
               />
             </div>
 
-            <button type="submit" className=" w-full mt-4 text-white bg-gradient-to-r from-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+             <button
+              type="submit"
+              className="w-full px-6 py-3 mt-4 text-2xl font-bold text-white transition-all duration-200 bg-red-600 rounded-lg hover:bg-red-500 hover:scale-105 xl:text-3xl"
+            >
               Book Reservation
             </button>
           </form>
